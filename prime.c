3402f839d959f924cd0429e7443e59e1dff0cc12
ShriDhathri <120187644+ShriDhathri@users.noreply.github.com>
2023-02-10 19:00:11 +0530

    done

---
+++ prime.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n); 
+	if(n % 2 != 0)
+	{
+	printf("number %d is a prime number\n",n);
+	}
+	else
+	printf("number %d is not a prime number\n",n);
+}
+
+

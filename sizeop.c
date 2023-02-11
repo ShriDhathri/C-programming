2a88ad9f94b815d39f137f86f9a0ee94b09282a5
ShriDhathri <120187644+ShriDhathri@users.noreply.github.com>
2023-02-11 19:41:55 +0530

    done

---
+++ sizeop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int x;
+char c;
+float y;
+double z;
+printf("size of int = %d\n",sizeof(x));
+printf("size of char = %d\n",sizeof(c));
+printf("size of float = %d\n",sizeof(y));
+printf("size of double = %d\n",sizeof(z));
+}
+
